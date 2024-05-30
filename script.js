448e67d4b8a39446deee363fd23501c98406b978
Moises Henrique <moihgm67@gmail.com>
2024-05-30 20:29:57 -0300

    Atualizando Projeto

--- script.js
+++ script.js
@@ -6,11 +6,7 @@ function validarCampo() {
     if(Valor.value == '' || Desconto.value == '') {
         window.alert('Digite um valor para calcular');
         return false;
-    } else {
-        Valor.value == 0 || Desconto.value == 0
-        window.alert('Digite um valor acima de 0');
-        return false;
-    }
+    } 
     return true;
 }
 
@@ -18,21 +14,21 @@ function calcularDesconto () {
   let PreçoOriginal = parseFloat(Valor.value);
   let percetualDesconto = parseFloat(Desconto.value);
 
-  if (percetualDesconto > 0 || percetualDesconto > 100) {
-     window.alert('digite um valor valido para desconto!!');
-     return;
+
+if (percetualDesconto > 0 || percetualDesconto > 100) {
+    window.alert('digite um valor valido para desconto!!');
+    return;
 }
+
  let DescontoFinal = (percetualDesconto / 100) * PreçoOriginal
  let PrecoFinal = PreçoOriginal 
 
- for (let i = 0; i < DescontoFinal; i += 0.01){
-    PrecoFinal += 0.01;
+ Resultado.textContent = 'O preço final com desconto é: ' + PreçoOriginal.toFixed();
 }
- Resultado.textContent = 'O preço final com desconto é: ' + precoFinal.toFixed(2);
-}
+
 document.querySelector('.Button-Calcular').addEventListener('click', function(){
     if (validarCampo()) {
         calcularDesconto();
-     };
+    } 
 });
     
